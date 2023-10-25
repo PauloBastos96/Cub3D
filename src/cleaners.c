0d5234a4ccff9433925c2e679c7f6cf873e5305a
Paulo Bastos <paulorod@student.42lisboa.fr>
2023-10-25 15:12:13 +0100

    Removed redundant function free_map.

--- src/cleaners.c
+++ src/cleaners.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cleaners.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paulorod <paulorod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:27:19 by paulorod          #+#    #+#             */
-/*   Updated: 2023/10/25 14:00:23 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2023/10/25 15:11:17 by paulorod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,6 @@ void	free_split(char **split)
 	free(split);
 }
 
-/// Free the map
-/// @param cub Cub struct
-void	free_map(t_cub *cub)
-{
-	int	i;
-
-	i = 0;
-	if (!cub->map)
-		return ;
-	while (cub->map[i])
-	{
-		free(cub->map[i]);
-		i++;
-	}
-	free(cub->map);
-}
-
 /// Free the textures and colors
 /// @param cub Cub struct
 void	free_colors(t_cub *cub)
@@ -52,13 +35,14 @@ void	free_colors(t_cub *cub)
 }
 
 /// Clean the game and exit
-/// @param cub 
+/// @details The textures are freed along with cub->file
+/// @param cub The cub struct
 void	exit_game(t_cub *cub)
 {
 	free_split(cub->file);
-	free_map(cub);
-	free_colors(cub);
+	free_split(cub->map);
 	free(cub->player);
+	free_colors(cub);
 	free(cub);
 	exit(0);
 }
